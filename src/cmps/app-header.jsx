d44732139327c54470598bcf591933345e2dd726
Rotem Spivak <102173457+RotemSpivak@users.noreply.github.com>
2022-05-25 00:50:46 +0300

    home page

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom"
-
+import logo from "../imgs/logo.png"
 
 export const AppHeader = () => {
     return <div className="app-header">
-        <h1>Traillo</h1>
+        <div className="logo-container">
+            <img className="logo" src={logo}/>
+            <h1>Traillo</h1>
+        </div>
+       
         <div className="app-header-links">
         <NavLink className="nav-link" to='/boards'>Boards</NavLink>
         <NavLink className="nav-link login" to='/login'>Login</NavLink>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+import image from "../imgs/home-image.png"
 
 
 export const Home = () => {
 
     return <div className="home"> 
-    <h1>Traillo makes managing projects easy.</h1>
-    <p>Take productivity to another level, from business to home. Whether you collab with others or work alone, you can accomlpish anything with Traillo.</p>
+    <div className="home-texts">
+    <h1>Traillo - management made easy.</h1>
+    <p>Take productivity to another level, from business to personal use. Whether you collab with others or work alone, you can accomplish anything with Traillo.</p>
     <NavLink to="/signup"><button>Sign up-it's free!</button></NavLink>
+    </div>
+    <div className="img-container">
+        <img src={image}  />
+    </div>
 
     </div>
 }
+// "https://www.kindpng.com/picc/m/663-6630367_transparent-background-teamwork-clipart-hd-png-download.png"
