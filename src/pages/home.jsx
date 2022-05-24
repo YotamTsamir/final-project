3352b0710a4c3ee266963492e1def41608a00886
Rotem Spivak <102173457+RotemSpivak@users.noreply.github.com>
2022-05-24 19:53:36 +0300

    home-page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+
 
 export const Home = () => {
+
     return <div className="home"> 
-    <h1>Welcome to Traillo</h1>
-    <aside>Make managing projects easy.</aside>
+    <h1>Traillo makes managing projects easy.</h1>
+    <p>Take productivity to another level, from business to home. Whether you collab with others or work alone, you can accomlpish anything with Traillo.</p>
+    <NavLink to="/signup"><button>Sign up-it's free!</button></NavLink>
+
     </div>
 }
--- src/store/reducer/user-reducer.js
+++ src/store/reducer/user-reducer.js
@@ -1,12 +1,15 @@
+import { userService } from "../../services/user-service";
+
 const initialState = {
+    // user: userService.getLoggedinUser(),
 }
 export function userReducer(state = initialState, action) {
     var newState = state;
     switch (action.type) {
-        // case 'SET_BOARD':
-            // newState = { board:action.board }
-            // break;
+        case 'SET_USER':
+            return { ...state, user: action.user }
         default:
+            return state
     }
 
     return newState;
