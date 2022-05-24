307b91e0db2c7015ba36d8a3e23842eb7e2188bb
Rotem Spivak <102173457+RotemSpivak@users.noreply.github.com>
2022-05-24 20:53:02 +0300

    more stuff

--- src/cmps/app-header.jsx
+++ src/cmps/app-header.jsx
@@ -5,8 +5,9 @@ export const AppHeader = () => {
     return <div className="app-header">
         <h1>Traillo</h1>
         <div className="app-header-links">
-        <NavLink className="nav-link" to='/login'>Login</NavLink>
         <NavLink className="nav-link" to='/boards'>Boards</NavLink>
+        <NavLink className="nav-link login" to='/login'>Login</NavLink>
+        <NavLink className="nav-link signup" to='/signup'>Sign up</NavLink>
 
         </div>
     </div>
