3d6f27ec5d1d0fac6c94a9de04a2915f5efb0f21
ShahaerCohenVolos <shaharc4@gmail.com>
2022-05-28 23:25:53 +0300

    d

--- src/cmps/details-task-nav.jsx
+++ src/cmps/details-task-nav.jsx
@@ -31,37 +31,34 @@ export const DetailsTaskNav = ({ board, task, box, onEditTaskTitle }) => {
     }
 
     const menuBtns = [
-        { txt: 'Labels'},
-        { txt: 'Cover'},
-        { txt: 'Members'},
-        { txt: 'Move'},
-        { txt: 'Copy'},
-        { txt: 'Dates'},
-        { txt: 'Archive'},
-    ].filter(({txt}) => !(task.color && txt === 'Cover'))
+        { txt: 'Labels' },
+        { txt: 'Cover' },
+        { txt: 'Members' },
+        { txt: 'Move' },
+        { txt: 'Copy' },
+        { txt: 'Dates' },
+        { txt: 'Archive' },
+    ].filter(({ txt }) => !(task.color && txt === 'Cover'))
 
-    const colors=['#7BC86C','#F5DD29', '#EF7564', '#CD8DE5', '#5BA4CF','#29CCE5','#6DECA9','orange','#FF8ED4', '#8675A9']
+    const colors = ['#7BC86C', '#F5DD29', '#EF7564', '#CD8DE5', '#5BA4CF', '#29CCE5', '#6DECA9', 'orange', '#FF8ED4', '#8675A9']
 
     return <section>
         <div className='add-to-card'>Add to card</div>
         <div className="details-task-nav">
             {menuBtns.map(btn => {
                 return (
-                    <button key={btn.txt} className="details-task-nav-btn" onClick={() => {toggleMenu(btn.txt)}}>
+                    <button key={btn.txt} className="details-task-nav-btn" onClick={() => { toggleMenu(btn.txt) }}>
                         {btn.txt}
                         {(menuState['Labels']) && btn.txt === 'Labels' && <LabelMenu topic={'Labels'} setIsEdit={setIsEdit} onEditTaskTitle={onEditTaskTitle} task={task} box={box} board={board} />}
-                        {(menuState['Cover']) && btn.txt === 'Cover' && <LabelMenu topic={'Cover'} colors={colors} task={task} box={box} board={board}/>}
-                        { (menuState['Dates']) && btn.txt === 'Dates' && <LabelMenu topic={'Dates'} task={task} box={box} board={board} emitDateValue={emitDateValue}/>}
+                        {(menuState['Cover']) && btn.txt === 'Cover' && <LabelMenu topic={'Cover'} colors={colors} task={task} box={box} board={board} />}
+                        {(menuState['Dates']) && btn.txt === 'Dates' && <LabelMenu topic={'Dates'} task={task} box={box} board={board} emitDateValue={emitDateValue} />}
                         {(menuState['Members']) && btn.txt === 'Members' && <LabelMenu topic={'Members'} task={task} box={box} board={board} />}
                     </button>
                 )
             })}
         </div>
-            {(menuState['Dates']) && 
-            <button onClick={(ev) => { toggleEditTask()}}>Save</button>
+        {(menuState['Dates']) &&
+            <button onClick={(ev) => { toggleEditTask() }}>Save</button>
         }
-
-
-        
-        </section>
+    </section>
 }
